9f6432e8a3f0eb64a6d924e1bf77aefeb2b1bc77
RehtiW <Etranx@outlook.com>
2024-11-12 19:18:02 +0800

    committed lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t feedback = (*reg >> 0) & 1 ^ (*reg >> 2) & 1 ^ (*reg >> 3) & 1 ^ (*reg >> 5) & 1;
+    (*reg >> 1) | (feedback << 15);
 }
 
