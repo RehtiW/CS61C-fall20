cd7c0f08728d92ca6b8a38d4d598a13939b3321f
RehtiW <Etranx@outlook.com>
2024-11-29 22:22:46 +0800

    done with lab04

--- lab04/megalistmanips.s
+++ lab04/megalistmanips.s
@@ -44,12 +44,13 @@ main:
 
     li a0, 10
     ecall
-
+#a0 ,s0 address of this node
+# correctness:70,73,83,mystery
 map:
     addi sp, sp, -12
     sw ra, 0(sp)
     sw s1, 4(sp)
-    sw s0, 8(sp)
+    sw s0, 8(sp)      #push first node address into stack
 
     beq a0, x0, done    # if we were given a null pointer, we're done.
 
@@ -66,20 +67,20 @@ map:
     # are modified by the callees, even when we know the content inside the functions 
     # we call. this is to enforce the abstraction barrier of calling convention.
 mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
-    lw t2, 4(s0)        # load the size of the node's array into t2
-
-    add t1, t1, t0      # offset the array address by the count
-    lw a0, 0(t1)        # load the value at that address into a0
+    lw t1,0(s0)      #当前节点的数组地址# load the address of the array of current node into t1
+    lw t2, 4(s0)        #当前数组大小# load the size of the node's array into t2
+    slli t3, t0, 2      # t0:计数器
+    add t1, t1, t3      # offset the array address by the count
+    lw a0, 0(t1)        # a0:当前地址数组元素的大小# load the value at that address into a0
 
     jalr s1             # call the function on that value.
 
     sw a0, 0(t1)        # store the returned value back into the array
-    addi t0, t0, 1      # increment the count
+    addi t0, t0, 1      # increment the count 
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    add a1,s1,x0      # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
@@ -90,8 +91,12 @@ done:
     jr ra
 
 mystery:
+    addi sp,sp,-4
+    sw t1,0(sp)
     mul t1, a0, a0
     add a0, t1, a0
+    lw t1,0(sp)
+    addi sp,sp,+4
     jr ra
 
 create_default_list:
